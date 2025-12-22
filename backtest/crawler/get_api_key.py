2d69f41bc8b84fbce6ae45d576d88ec9594d4355
guzus <50664161+guzus@users.noreply.github.com>
2025-12-22 23:39:09 +0900

    Implement polymarket backtest (#24)
    * Adds Polymarket data fetching capabilities
    
    Adds a PolymarketFetcher class with methods to retrieve market data, price history, and trade information.
    
    This allows backtesting strategies against historical Polymarket data.
    Includes methods for:
    - Fetching price history
    - Searching markets based on various criteria
    - Retrieving public trade history
    
    Also includes example usage in a test notebook.
    
    upgrade search_market to given docs
    
    remove inherited class and move it to original class
    
    bug fix polymarket.py & implement backtest structure
    
    implement crawler & data visualize
    
    crawler & fetch data examples
    
    * chore
    
    * feat(polymarket): add AWS S3 support and improve tick size handling
    
    * chore(deps): add matplotlib dependency
    
    ---------
    
    Co-authored-by: Minkyun Seo <shane021212@snu.ac.kr>

---
+++ backtest/crawler/get_api_key.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+from py_clob_client.client import ClobClient
+
+load_dotenv()
+
+host: str = "https://clob.polymarket.com"
+key: str = os.getenv("POLYMARKET_PRIVATE_KEY")  # This is your Private Key.
+chain_id: int = 137  # No need to adjust this
+POLYMARKET_PROXY_ADDRESS: str = os.getenv(
+    "POLYMARKET_FUNDER"
+)  # This is the address you deposit/send USDC to to FUND your Polymarket account.
+
+# Select from the following 3 initialization options to matches your login method, and remove any unused lines so only one client is initialized.
+
+### Initialization of a client using a Polymarket Proxy associated with an Email/Magic account. If you login with your email use this example.
+client = ClobClient(
+    host, key=key, chain_id=chain_id, signature_type=1, funder=POLYMARKET_PROXY_ADDRESS
+)
+
+### Initialization of a client using a Polymarket Proxy associated with a Browser Wallet(Metamask, Coinbase Wallet, etc)
+client = ClobClient(
+    host, key=key, chain_id=chain_id, signature_type=2, funder=POLYMARKET_PROXY_ADDRESS
+)
+
+### Initialization of a client that trades directly from an EOA.
+client = ClobClient(host, key=key, chain_id=chain_id)
+
+print(client.derive_api_key())
